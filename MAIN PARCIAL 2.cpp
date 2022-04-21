b45db48a288e02a6ddd3cc122da834f540e98032
Molotov35 <emontejom1@miumg.edu.gt>
2022-04-21 13:55:52 -0600

    actualizacion del main

--- MAIN PARCIAL 2.cpp
+++ MAIN PARCIAL 2.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
 
+
+#include "LISTAR USUARIOS.h"
 #include "MENUS.h"
 
 using namespace std;
 
 int main()
 {
-	Menu inicio;
-	inicio.MenuInicial();
+	int encendido=1;
+	while (encendido==1)
+	{
+		int opc;
+		Menu inicio;
+		opc=inicio.MenuInicial();
+		cout << opc;
+		switch (opc) {
+			case 1:
+				usuarios lista;
+				lista.menuListarUsuarios();
+				encendido =1;
+			case 2:
+				//menuRegistrarUsuario();
+				break;
+	
+			case 3:
+				//menuIniciarSesion();
+				break;
+
+			case 0:
+				exit;
+		}
+	}
 	return 0;
 }
